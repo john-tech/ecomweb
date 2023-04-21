949eb39d9312215dac4cdb9e4cfbd82b83761a1d
Asif Aslam <asifaslamaimviz@gmail.com>
2023-04-22 00:38:31 +0500

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,9 +6,9 @@ import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row md:justify-start  justify-center items-center py-2 shadow-xl'>
+    <div className='flex flex-col md:flex-row md:justify-start  justify-center items-center py-2 shadow-md'>
         <div className="logo mx-5">
-            <Image src="/logo.png" alt="" width={60} height={60}/>
+           <Link href={'/'}> <Image src="/logo.png" alt="" width={60} height={60}/></Link>
         </div>
             <div className="nav">
                 <ul className='flex items-center space-x-8 font-bold md:text-md'>
